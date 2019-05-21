5b9ba8e420d121edbe6a253f8abcb71146f935a8
Edgar Gil <edgardasun@gmail.com>
2019-05-20 23:12:02 -0400

    Updates

---
+++ precache-manifest.228413290fd115925fcd401286882df0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc39d6b0116e955da5f39f888c031488",
+    "url": "/edserver/shaquille/index.html"
+  },
+  {
+    "revision": "b6c00172c9182cdd5005",
+    "url": "/edserver/shaquille/static/css/main.76e4a170.chunk.css"
+  },
+  {
+    "revision": "755cce630fbbf5fc1ccd",
+    "url": "/edserver/shaquille/static/js/2.8bb8a4ea.chunk.js"
+  },
+  {
+    "revision": "b6c00172c9182cdd5005",
+    "url": "/edserver/shaquille/static/js/main.a3104d52.chunk.js"
+  },
+  {
+    "revision": "0d2fcace2c825e2736f3",
+    "url": "/edserver/shaquille/static/js/runtime~main.5e030f79.js"
+  },
+  {
+    "revision": "aff2d89c724847caf04d384d3bf9f2c8",
+    "url": "/edserver/shaquille/static/media/1.aff2d89c.jpg"
+  },
+  {
+    "revision": "8ac5f5bfd59ed54feedb8faf8e1fbf37",
+    "url": "/edserver/shaquille/static/media/2.8ac5f5bf.jpg"
+  },
+  {
+    "revision": "e647bd0e3058c95bce28f968340662c7",
+    "url": "/edserver/shaquille/static/media/3.e647bd0e.jpg"
+  },
+  {
+    "revision": "775c772e63a54ee9c4787d27d5a0b7a1",
+    "url": "/edserver/shaquille/static/media/4.775c772e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/edserver/shaquille/static/media/logo.5d5d9eef.svg"
+  }
+]);
